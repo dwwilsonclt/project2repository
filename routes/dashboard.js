ea446eeae78ad5245771992e88e4eeb1413b2938
Luis Paredes <lparedesl@gmail.com>
2017-06-10 23:05:16 -0400

    Create dashboard.js

--- routes/dashboard.js
+++ routes/dashboard.js
@@ -4,10 +4,22 @@ var router = express.Router();
 router.get("/", isLoggedIn, function(req, res, next) {
     if (req.session.userType === "Admin") {
         res.end("this is the Admin homepage");
+        // here we will render the Admin homepage
+        // each button/link inside this homepage will have admin in front
+        // now we know that all routes that begin with admin (localhost:8080/dashboard/admin/...)
+        // are for an Admin view
     } else if (req.session.userType === "Professor") {
         res.end("This is the professor homepage");
+        // here we will render the Professor homepage
+        // each button/link inside this homepage will have professor in front
+        // now we know that all routes that begin with professor (localhost:8080/dashboard/professor/...)
+        // are for a Professor view
     } else if (req.session.userType === "Student") {
         res.end("This is the Student homepage");
+        // here we will render the Student homepage
+        // each button/link inside this homepage will have student in front
+        // now we know that all routes that begin with student (localhost:8080/dashboard/student/...)
+        // are for a Student view
     }
 });
 
