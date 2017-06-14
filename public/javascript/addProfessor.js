66de685273bb5ad50c6b4bc4d1f39f2605e3bedc
Dan Phillips <dlphillips@gmail.com>
2017-06-14 14:16:29 -0400

    adding building

---
+++ public/javascript/addProfessor.js
@@ -0,0 +1,16 @@
+$(document).ready(function() {
+    $("#building").on("change", function() {
+        $.post("/dashboard/admin/departments/departments/add-professor/setBldg", {
+            building_id:  $("#building").val()
+        })
+        .done(function(rooms) {
+            $("#room").empty();
+            rooms.forEach(function(room) {
+                var option = $("<option>")
+                .attr("value", room.id)
+                .text(room.building.name + " " + room.room_number);
+                $("#room").append(option);
+            });
+        });
+    });
+});
